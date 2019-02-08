371e49b5d129c81d24a3086225bd6eeff2c66d57
Jovl <jeremiahlantzer25@gmail.com>
2019-02-08 18:48:24 -0500

    First test of deployment to Heroku with Firebase connection

--- server.py
+++ server.py
@@ -1,14 +1,24 @@
 from flask import Flask, jsonify, request
 from firebase import firebase
+import os
 
 app = Flask(__name__)
-fb = firebase.FirebaseApplication('https://.firebaseio.com/', None)
+fb = firebase.FirebaseApplication('https://hackriddleworkshop.firebaseio.com/', None)
 
 
-@app.route('/', methods=['POST'])
+@app.route('/add-user', methods=['POST'])
 def add_user():
     user_data = request.get_json(force=True)
     response = fb.put('/users', user_data['email'], user_data)
     return jsonify(response)
 
 
+@app.route("/get-users")
+def get_users():
+    response = fb.get('/users', None)
+    return jsonify(response)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)  # production IP
