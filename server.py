43f5265e42061351601d645a1a7da58033d30e53
Jovl <jeremiahlantzer25@gmail.com>
2019-02-27 13:08:06 -0500

    No CORS

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 from flask import Flask, jsonify, request
 from firebase import firebase
 import os
-from flask_cors import CORS
+#from flask_cors import CORS
 
 app = Flask(__name__)
 fb = firebase.FirebaseApplication(
     'https://hackriddleworkshop.firebaseio.com/', None)
 
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
+#cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.route('/add-user', methods=['POST'])
